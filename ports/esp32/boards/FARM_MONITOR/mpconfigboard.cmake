94fc4fa5f4d48096ceb7db9381c8a6e4d0214552
Danil Ruban <intx82@gmail.com>
2024-04-07 06:52:35 +0200

    adds build gsm0710 (goes in original esp32-mpy-uclient)

--- ports/esp32/boards/FARM_MONITOR/mpconfigboard.cmake
+++ ports/esp32/boards/FARM_MONITOR/mpconfigboard.cmake
@@ -7,4 +7,11 @@ set(SDKCONFIG_DEFAULTS
     boards/ESP32_GENERIC_C3/sdkconfig.c3usb
 )
 
+
+list(APPEND MICROPY_SOURCE_PORT
+    boards/FARM_MONITOR/gsm0710/buffer.c
+    boards/FARM_MONITOR/gsm0710/gsm0710.c
+    boards/FARM_MONITOR/gsm0710/mpy.c
+)
+
 set(MICROPY_FROZEN_MANIFEST ${MICROPY_BOARD_DIR}/manifest.py)
